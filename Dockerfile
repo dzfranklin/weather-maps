484f1369379b175667354d95f78ceee73a06dc1a
Daniel Franklin <daniel@danielzfranklin.org>
2025-01-15 20:17:54 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# syntax=docker/dockerfile:1
+FROM ghcr.io/osgeo/gdal:ubuntu-full-3.10.1
+
+RUN apt update && apt install -y git pipx
+
+RUN PIPX_BIN_DIR=/usr/bin pipx install --suffix _dwd git+https://github.com/DeutscherWetterdienst/downloader.git@392676084621282d4b2cb2f5a0e5ba5944ce69d0
+
+COPY . /weather-maps
---
+++ icon_eu_h_snow.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -euox pipefail
+
+rm -rf /prepare_icon_eu_h_snow && mkdir -p /prepare_icon_eu_h_snow && cd /prepare_icon_eu_h_snow
+
+mkdir -p data
+mkdir -p out
+
+downloader_dwd \
+    --directory ./data --grid regular-lat-lon \
+    --model icon-eu --single-level-fields h_snow \
+    --timestamp "$(date -u '+%Y-%m-%d')T00:00:00"\
+    --min-time-step 0 --max-time-step 120 --time-step-interval 24
+
+ls ./data > out/sources.txt
+
+for f in ./data/*; do
+  [ -e "$f" ] || continue
+
+  name=$(basename -- "$f" | sed -r "s/icon-eu_europe_regular-lat-lon_single-level_[0-9]+_([0-9]{3})_H_SNOW.grib2/\1h/")
+
+  echo "Processing $name"
+
+  gdaldem color-relief -alpha "$f" /weather-maps/h_snow_colormap.txt "${name}_color.tif"
+
+  gdal2tiles --zoom=1-5 --tilesize=512 --xyz "${name}_color.tif" "out/${name}"
+
+cat <<EOF >"out/${name}/source.json"
+{
+  "tiles": ["https://plantopo-storage.b-cdn.net/weather-maps/icon_eu_h_snow/${name}/{z}/{x}/{y}.png"],
+  "minzoom": 1,
+  "maxzoom": 5,
+  "attribution": "<a href=\"https://www.dwd.de/EN/ourservices/nwp_forecast_data/nwp_forecast_data.html\" target=\"_blank\">Deutscher Wetterdienst</a>"
+}
+EOF
+
+done
+
+for f in $(find out -type f -printf "%P\n" | sort); do
+  curl -X PUT -H "AccessKey: $BUNNY_STORAGE_KEY" --fail-with-body \
+    "https://uk.storage.bunnycdn.com/plantopo/weather-maps/icon_eu_h_snow/$f" \
+    --data-binary "@out/$f"
+done
+
+for f in $(find out -type f -printf "%P\n" | sort); do
+  curl --get -H "AccessKey: $BUNNY_KEY" --fail-with-body "https://api.bunny.net/purge" \
+    -d "url=https://plantopo-storage.b-cdn.net/weather-maps/icon_eu_h_snow/$f"
+done
+
+echo "All done"
